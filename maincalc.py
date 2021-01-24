4e3c405f9474a5385f789d0a3d30c039d85888be
Jayanti Acharya <jayanti.hello500@gmail.com>
2021-01-24 16:14:35 +0545

    work completed

---
+++ maincalc.py
@@ -0,0 +1,14 @@
+import addition
+import sub
+import mul
+import div
+
+a = 1000
+b = 100
+
+total_sum = addition.add(a,b)
+total_sub = sub.sub(a,b)
+total_mul = mul.mul(a,b)
+total_div = div.div(a,b)
+
+print(f"total sum is : {total_sum} \n total sub is {total_sub} \n total mul is : {total_mul} \n total div is {total_div}")
